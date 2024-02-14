6596b0ccfb0d0b38583ace34c895d01473589631
FellipeCastro <fellipecastro27d@gmail.com>
2024-02-14 19:15:29 -0300

    aula 04 finalizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
 import './App.css'
+import './Sidebar.css'
+import './Main.css'
+import Notes from './components/Notes'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div id='app'>
+      <aside>
+        <strong>Caderno de notas</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="title">Título da anotação</label>
+            <input type="text" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="nota">Anotações</label>
+            <textarea></textarea>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+        </ul>
+      </main>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Notes.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Notes() {
+    return (
+        <>
+            <li className='notepad-infos'>
+                <div>
+                <strong>Fazer compras</strong>
+                <div>
+                    x
+                </div>
+                <textarea>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, repellendus sit. Omnis fugit consectetur facere quam voluptatibus voluptates totam aperiam similique, architecto error aut porro qui necessitatibus soluta accusamus numquam?
+                </textarea>
+                <span>!</span>
+                </div>
+            </li>
+        </>
+    )
+}
+
+export default Notes
