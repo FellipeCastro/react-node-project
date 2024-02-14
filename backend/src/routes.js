be319ff5fae99d45c802b37892e57e4a69dddef0
FellipeCastro <fellipecastro27d@gmail.com>
2024-02-14 10:55:36 -0300

    aula 02 pt2 finalizada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 const AnnotationController = require('./controllers/AnnotationController.js')
+const PriorityController = require('./controllers/PriorityController.js')
+const ContentController = require('./controllers/ContentController.js')
 
 // Rota Annotation
 routes.get('/annotations', AnnotationController.read)
 routes.post('/annotations', AnnotationController.create)
 routes.delete('/annotations/:id', AnnotationController.delete)
+
+//Rota Priority
+routes.get('/priorities', PriorityController.read)
+routes.post('/priorities/:id', PriorityController.update)
+
+// Rota Content
+routes.post('/contents/:id', ContentController.update)
 
 module.exports = routes
