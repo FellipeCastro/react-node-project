62e139b8878b4e34330a3d03559f724c915b44d6
FellipeCastro <fellipecastro27d@gmail.com>
2024-02-15 19:04:36 -0300

    aula 05

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,56 @@
+import { useState, useEffect } from 'react'
+
+import api from './services/api'
+
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
+
 import Notes from './components/Notes'
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [notes, setNotes] = useState('')
+  const [allNotes, setAllNotes] = useState([])
+
+  useEffect(() => {
+    const getAllNotes = async () => {
+      const res = await api.get('/annotations')
+
+      setAllNotes(res.data)
+    }
+
+    getAllNotes()
+  }, [title, notes])
+
+  const hanldeSubit = async (e) => {
+    e.preventDefault()
+
+    const res = await api.post('/annotations', {
+      title,
+      notes,
+      priority: false
+    })
+
+    setTitle('')
+    setNotes('')
+
+    setAllNotes([...allNotes, res.data])
+  }
 
   return (
     <div id='app'>
       <aside>
         <strong>Caderno de notas</strong>
-        <form>
+        <form onSubmit={hanldeSubit}>
           <div className="input-block">
             <label htmlFor="title">Título da anotação</label>
-            <input type="text" />
+            <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
           </div>
 
           <div className="input-block">
             <label htmlFor="nota">Anotações</label>
-            <textarea></textarea>
+            <textarea onChange={(e) => setNotes(e.target.value)} value={notes} required></textarea>
           </div>
 
           <button type="submit">Salvar</button>
@@ -26,15 +59,14 @@ function App() {
 
       <main>
         <ul>
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
-          <Notes />
+          {allNotes.map(data => {
+            return (
+              <Notes
+                data={data}
+              />
+            )
+
+          })}
         </ul>
       </main>
     </div>
--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,18 +1,16 @@
 import React from "react"
 
-function Notes() {
+function Notes({ data }) {
     return (
         <>
             <li className='notepad-infos'>
                 <div>
-                <strong>Fazer compras</strong>
-                <div>
-                    x
-                </div>
-                <textarea>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, repellendus sit. Omnis fugit consectetur facere quam voluptatibus voluptates totam aperiam similique, architecto error aut porro qui necessitatibus soluta accusamus numquam?
-                </textarea>
-                <span>!</span>
+                    <strong>{data.title}</strong>
+                    <div>
+                        x
+                    </div>
+                    <textarea defaultValue={data.notes}></textarea>
+                    <span>!</span>
                 </div>
             </li>
         </>
